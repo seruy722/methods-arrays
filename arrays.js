c7f48f29683dad43f5dd8b558df68a681c442fe0
SergeyDenisiuk <seruy722@gmail.com>
2017-05-27 17:30:37 +0300

    map

--- arrays.js
+++ arrays.js
@@ -12,3 +12,16 @@ each(arr, item => console.log(item * 2));
 let links = document.links;
 
 each([...links], link => link.classList.add('link-active'));
+
+// map
+
+function map(arr, callback, thisArg) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(callback.call(thisArg, arr[i], i, arr));
+    }
+    return result;
+}
+
+let href = map([...links], link => link.href);
+console.log(href);
